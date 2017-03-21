c1cd11415a0e1b31d1244781b65c0a94180e57d3
James Proett <jamesproett@JAP.local>
2017-03-21 09:13:16 -0500

    started it

---
+++ pets.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+if (process.argv.length === 2) {
+    console.error;
+    console.log('Usage: node pets.js [read | create | update | destroy]');
+}
+if (process.argv[2] === 'read') {
+    console.log('fhefwbhewf');
+}
